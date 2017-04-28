9bf6bec4cc35550c682639f9ce21f529b33970ab
daniel2005d <daniel.vargas@outlook.com>
2017-04-28 17:54:43 -0500

    Add files via upload

--- rest-api-bruteforcing.py
+++ rest-api-bruteforcing.py
@@ -7,19 +7,35 @@ parser = argparse.ArgumentParser(description='Intenta descubri mediante fuerza b
 parser.add_argument('--url', dest='Url')
 parser.add_argument('--file',dest='Diccionario')
 
+print("""
+                 _   _____  ______  _____ _______ 
+     /\         (_) |  __ \|  ____|/ ____|__   __|
+    /  \   _ __  _  | |__) | |__  | (___    | |   
+   / /\ \ | '_ \| | |  _  /|  __|  \___ \   | |   
+  / ____ \| |_) | | | | \ \| |____ ____) |  | |   
+ /_/    \_\ .__/|_| |_|  \_\______|_____/   |_|   
+          | |                                     
+          |_|                                     
+
+""")
+print('')
 args = parser.parse_args()
 servicios = open(args.Diccionario)
 for s in servicios:
-    try:
-       api = args.Url + s
-       response = requests.get(api.strip(), verify=True)
-       if (response.ok):
-           print('{}[0;93m {}-[OK] {}[0m'.format(chr(27),args.Url, chr(27)))
-       else:
-           print('{}[0;91m {}-Failed {}[0m'.format(chr(27),args.Url, chr(27)))
-    except Exception as ex:
-           print(chr(27) + '[0;31m' +ex + chr(27)+'[0m')
-           pass
+    if s:
+        try:
+           api = args.Url + s
+           response = requests.get(api.strip(), verify=True)
+
+           if (response.ok):
+              print('{}[0;93m {}-[OK] {}[0m'.format(chr(27),api, chr(27)))
+           else:
+              print('{}[0;91m {}-[failed] {}[0m'.format(chr(27),api.strip(), chr(27)))
+              print('{}[0;93m {}'.format(chr(27),response.json()))
+        except Exception as ex:
+              print(chr(27) + '[0;31m' +ex + chr(27)+'[0m')
+              print(response)
+              pass
 
 
 
